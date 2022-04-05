c5b2dddc5884dd8fc2d842dbc4c8dd3024bdff5e
DanLevyM <dan.levy.pro@gmail.com>
2022-04-05 16:24:38 +0200

    update todos

--- cypress/integration/todos/todos.spec.js
+++ cypress/integration/todos/todos.spec.js
@@ -1,14 +1,11 @@
-function createMyTodoList() {
-  cy.get('input')
-    .type('Learn Cypress {enter}')
-    .type('Learn React {enter}')
-    .type('Learn Node {enter}')
-    .type('Learn to code {enter}');
-}
-
 describe('to-do app', () => {
   beforeEach(() => {
     cy.visit('./index.html');
+    cy.get('input')
+      .type('Learn Cypress {enter}')
+      .type('Learn React {enter}')
+      .type('Learn Node {enter}')
+      .type('Learn to code {enter}');
   });
 
   it('should contain 1 form', () => {
@@ -20,30 +17,50 @@ describe('to-do app', () => {
   });
 
   it('create 4 todos', () => {
-    createMyTodoList();
+    cy.get('ul li').should('have.length', 4);
   });
 
   it('should contain 4 todos ', () => {
-    createMyTodoList();
     cy.get('ul li').should('have.length', 4);
   });
 
   it('complete all todos', () => {
-    createMyTodoList();
     cy.get('input[type=checkbox]').check();
     cy.get('ul li').should('have.class', 'completed');
-  })
+  });
 
-  it('delete 1 todos', () => {
-    createMyTodoList();
+  it('delete 1 todo', () => {
     cy.get('ul li div').eq(1)
       .should('have.class', 'close')
       .click();
     cy.get('ul li').should('have.length', 3);
   })
 
-  /* it('update todo', () => {
-    
-  }) */
+  it('modal should not be visible when not updating todo', () => {
+    cy.get('.modal').should('not.exist')
+  })
 
-}); 
+  it('modal should be visible when updating todo', () => {
+    cy.get('.update:first').click();
+    cy.get('.modal').should('be.visible')
+  })
+
+  it('modal can be closed when updating todo', () => {
+    cy.get('.update:first').click();
+    cy.get('.modal-close').click();
+    cy.get('.modal').should('not.exist');
+  });
+
+  it('update todo', () => {
+    cy.get('.update:first').click();
+    cy.get('#input-modal').clear().type('Forget php{enter}');
+    cy.contains('Forget php');
+  });
+
+  it('should contain 4 todos after update', () => {
+    cy.get('.update:first').click();
+    cy.get('#input-modal').clear().type('Forget php{enter}');
+    cy.get('ul li').should('have.length', 4);
+  });
+
+});
